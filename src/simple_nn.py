fix gradient

src/simple_nn.py
@@ -5,12 +5,13 @@ from process_data import clean_word
 import json
 
 class SimpleNN:
-    def __init__(self):
+    def __init__(self, context_len = 6):
         self.nn_model = Sequential()
         self.train_data = ()
         self.data = np.array([])
         self.unique_words = np.array([])
         self.subsample_size = 7000
+        self.context_len = context_len
     
     def index_to_one_hot(self, index, length):
         one_hot = np.zeros(length)
@@ -22,8 +23,8 @@ class SimpleNN:
     
     def make_train_data(self, start, end):
         splits = []
-        for i in range(start, end-self.context_len-1):
-            splits.append(self.data[i:i+self.context_len+1])
+        for i in range(start, end - self.context_len - 1):
+            splits.append(self.data[i : i + self.context_len + 1])
         splits = np.array(splits)
         X_train = np.array([self.convert_input_to_embedding(s[:-1]) for s in splits])
         Y_train = np.array([self.index_to_one_hot(self.unique_words.tolist().index(s[-1]), len(self.unique_words)) for s in splits])
@@ -58,6 +59,9 @@ class SimpleNN:
         self.word_to_embedding = {word: np.array(embedding) for word, embedding in embedding_list.items()}
         self.unique_words = np.array(list(self.word_to_embedding.keys()))
         print("Embeddings loaded successfully!")
+
+    def convert_input_to_embedding(self, input):
+        return np.array([self.word_to_embedding[str(i)] for i in input])
 
 
 def generate_text(context, model, size):

src/word2vec.py
@@ -2,9 +2,9 @@ import numpy as np
 from collections import Counter
 import json
 
-
+#context_len == l
 class word2vec:
-    def __init__(self, embedding_size=100, context_len=6, neg_context_mul=2):
+    def __init__(self, embedding_size = 100, context_len = 6, neg_context_mul = 2):
         self.embedding_size = embedding_size
         self.context_len = context_len
         self.neg_context_mul = neg_context_mul
@@ -12,7 +12,6 @@ class word2vec:
         self.data = np.array([])
         self.count_to_words = {}
         self.unique_words = np.array([])
-        self.train_data = []
         
         self.subsample_size = 5000
         
@@ -21,9 +20,9 @@ class word2vec:
         return np.dot(c, w)
     
     def sigmoid(self, x):
-        return 1/(1+np.exp(-x))
+        return 1/(1 + np.exp(-x))
     
-    def add_data(self, new_data, count_words=True):
+    def add_data(self, new_data, count_words = True):
         self.data = np.concatenate((self.data, new_data))
         if(count_words):
             self.count_words_data()
@@ -36,16 +35,16 @@ class word2vec:
                 self.count_to_words[count] = set()
             self.count_to_words[count].add(word)
         
-    def find_neg_context(self, i, pos_context):
-        word = self.data[i]
-        neg_context_len = self.context_len*self.neg_context_mul*2
-        word_count = np.count_nonzero(self.data==word)
+    def find_neg_context(self, idx, pos_context):
+        word = self.data[idx]
+        neg_context_len = self.context_len * self.neg_context_mul
+        word_count = np.count_nonzero(self.data == word)
         neg_context = set()
         specific_counter = 0
         specific_sign = -1
 
         while len(neg_context) != neg_context_len:
-            word_count += specific_sign*specific_counter
+            word_count += specific_sign * specific_counter
             specific_counter += 1
             specific_sign = -specific_sign
             if word_count not in self.count_to_words:
@@ -53,34 +52,50 @@ class word2vec:
             for i in self.count_to_words[word_count]:
                 if i not in pos_context and i != word:
                     neg_context.add(i)
-                    if(len(neg_context) == neg_context_len):
+                    if len(neg_context) == neg_context_len:
                         break
+
+        return {word : self.word_to_embedding[word] for word in neg_context}
             
-        return np.array([self.word_to_embedding[i] for i in neg_context])
-            
-    def find_pos_context(self, i):
-        mask = (np.arange(len(self.data)) >= i - self.context_len) & (np.arange(len(self.data)) <= i + self.context_len)
-        mask &= (np.arange(len(self.data)) != i)
-        pos_context = np.array([self.word_to_embedding[i] for i in self.data[mask]])
+    def find_pos_context(self, idx):
+        mask = [x for x in range (max(0, idx - self.context_len), min(len(self.data), idx + self.context_len))]
+        mask.remove(idx)
+        mask = np.array(mask)
+        pos_context = {word: self.word_to_embedding[word] for word in self.data[mask]}
         return pos_context
     
-    def gradient_descent_iter(self, cur_weights, pos_context, neg_context, eta):
-        k = 0
-        for pos in pos_context:
-            k += 1
-            neg_grad_part = np.sum([self.sigmoid(self.similarity(neg, cur_weights))*neg for neg in neg_context], axis=0)
-            cur_weights -= eta*((self.sigmoid(self.similarity(pos, cur_weights))-1)*pos + neg_grad_part)
-        return cur_weights
-    
-    def logistic_regression(self, i, number_iterations, epsilon):
-        word = self.data[i]
-        pos_context = self.find_pos_context(i)
-        neg_context = self.find_neg_context(i, pos_context)
+    def gradient_descent_iter(self, word_vec, pos_context, neg_context, eta):
+        d_dword = np.array([0.0 for _ in range(self.embedding_size)])
+
+        for pos_word, vec_pos in pos_context.items():
+            d_dpos = (self.sigmoid(self.similarity(vec_pos, word_vec)) - 1) * word_vec
+            d_dword += (self.sigmoid(self.similarity(vec_pos, word_vec)) - 1) * vec_pos
+            pos_context[pos_word] -= eta * d_dpos
+
+        for neg_word, vec_neg in neg_context.items():
+            d_dneg = self.sigmoid(self.similarity(vec_neg, word_vec)) * word_vec
+            d_dword += self.sigmoid(self.similarity(vec_neg, word_vec)) * vec_neg
+            neg_context[neg_word] -= eta * d_dneg
+
+        word_vec -= eta * d_dword
+
+        return word_vec, pos_context, neg_context
+
+    def change_weight(self, context):
+        for word, vec_word in context.items():
+            self.word_to_embedding[word] = vec_word
+
+    def logistic_regression(self, idx, number_iterations, epsilon):
+        word = self.data[idx]
+        pos_context = self.find_pos_context(idx)
+        neg_context = self.find_neg_context(idx, pos_context)
         cur_weights = self.word_to_embedding[word]
         for i in range(number_iterations):
-            new_weights = self.gradient_descent_iter(cur_weights, pos_context, neg_context, 0.1)
-            if np.mean(np.abs(cur_weights-new_weights)) < epsilon:
+            new_weights, pos_context, neg_context = self.gradient_descent_iter(cur_weights, pos_context, neg_context, 0.1)
+            if np.abs(np.linalg.norm(cur_weights - new_weights)) < epsilon:
                 break
+        self.change_weight(neg_context)
+        self.change_weight(pos_context)
         return new_weights
         
     def convert_input_to_embedding(self, input):